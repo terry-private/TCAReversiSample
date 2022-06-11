4b7453b90f092af65435cb0eaee3e214a678aa65
teruhito <ataeteruhito@gmail.com>
2022-06-12 00:35:41 +0900

    Initial Commit

---
+++ Reversi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reversi
+//
+//  Created by 若江照仁 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by 若江照仁 on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
